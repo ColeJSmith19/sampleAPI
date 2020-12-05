eca1a13fa725eb8643ffce5eeb68af7e4b372d53
ColeSmith <colesmith1337@gmail.com>
2020-12-05 14:56:41 -0500

    path parameters added for /person endpoint. Response header set to application/json

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	_ "github.com/lib/pq"
 )
@@ -52,8 +53,8 @@ func main() {
 	fmt.Println("Listening on port 18080...")
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/people", getPeople)
-	http.HandleFunc("/person", personEndPoint)
-	// http.HandleFunc("/person/{first}/{last}", getPerson)
+	http.HandleFunc("/person/", getOrPostPerson)
+	// http.HandleFunc("/queryperson", queryPersonEndPoint)
 	log.Fatal(http.ListenAndServe(":18080", nil))
 }
 
@@ -61,13 +62,24 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
 }
 
-func personEndPoint(w http.ResponseWriter, r *http.Request) {
+func getOrPostPerson(w http.ResponseWriter, r *http.Request) {
+
+	path := strings.TrimPrefix(r.URL.Path, "/person/")
+	params := strings.Split(path, "/")
+
+	if len(params) != 2 {
+		http.Error(w, "Requests must include a last and a first name\n/person/:last/:first", 400)
+		return
+	}
+
+	lastName := params[0]
+	firstName := params[1]
 
 	switch r.Method {
 	case "GET":
-		getPersonQueryString(w, r)
+		getPerson(w, r, lastName, firstName)
 	case "POST":
-		postPerson(w, r)
+		postPerson(w, r, lastName, firstName)
 	default:
 		http.Error(w, "Must be a 'GET' or 'POST' request", 400)
 		return
@@ -75,7 +87,7 @@ func personEndPoint(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func postPerson(w http.ResponseWriter, r *http.Request) {
+func getPerson(w http.ResponseWriter, r *http.Request, last, first string) {
 
 	db, err := makeDBconnection()
 	if err != nil {
@@ -84,55 +96,7 @@ func postPerson(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	params := r.URL.Query()
-
-	last, lastok := params["last"]
-	first, firstok := params["first"]
-
-	if !lastok || !firstok {
-		http.Error(w, "Please include 'first' and 'last' as a query parameter", 400)
-		return
-	}
-
-	query := "insert into people (first, last) values ($1, $2)"
-	_, err = db.Exec(query, first[0], last[0])
-	if err != nil {
-		http.Error(w, "Something went wrong", 500)
-		return
-	}
-
-	json.NewEncoder(w).Encode(fmt.Sprintf("Successfully inserted %s %s", first[0], last[0]))
-
-}
-
-func getPersonQueryString(w http.ResponseWriter, r *http.Request) {
-
-	db, err := makeDBconnection()
-	if err != nil {
-		http.Error(w, "Something went wrong", 500)
-		return
-	}
-	defer db.Close()
-
-	params := r.URL.Query()
-
-	last, lastok := params["last"]
-	first, firstok := params["first"]
-
-	if !lastok && !firstok {
-		http.Error(w, "Please include 'first' and/or 'last' as a query parameter", 400)
-		return
-	}
-
-	query := ""
-
-	if lastok && firstok {
-		query = fmt.Sprintf("SELECT id, first, last from people where first = '%s' and last = '%s'", first[0], last[0])
-	} else if lastok {
-		query = fmt.Sprintf("SELECT id, first, last from people where last = '%s'", last[0])
-	} else {
-		query = fmt.Sprintf("SELECT id, first, last from people where first = '%s'", first[0])
-	}
+	query := fmt.Sprintf("SELECT id, last, first from people where first = '%s' and last = '%s'", first, last)
 
 	rows, err := db.Query(query)
 	if err != nil {
@@ -146,7 +110,7 @@ func getPersonQueryString(w http.ResponseWriter, r *http.Request) {
 
 	for rows.Next() {
 		var usr userDB
-		err = rows.Scan(&usr.ID, &usr.First, &usr.Last)
+		err = rows.Scan(&usr.ID, &usr.Last, &usr.First)
 		if err != nil {
 			fmt.Println(err)
 
@@ -157,57 +121,16 @@ func getPersonQueryString(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if len(allUsers) == 0 {
-		http.Error(w, "No data found", 404)
+		http.Error(w, fmt.Sprintf("/person/%s/%s does not exist", last, first), 404)
 		return
 	}
 
-	fmt.Println(allUsers)
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(allUsers)
-
-	fmt.Println("Success!")
 
 }
 
-// func getPerson(w http.ResponseWriter, r *http.Request) {
-
-// db, err := makeDBconnection()
-// if err != nil {
-// 	http.Error(w, "Something went wrong", 500)
-// 	return
-// }
-// defer db.Close()
-
-// 	rows, err := db.Query("SELECT id, first, last from people order by first desc limit 20")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	allUsers := make([]userDB, 0)
-
-// 	for rows.Next() {
-// 		var usr userDB
-// 		err = rows.Scan(&usr.ID, &usr.First, &usr.Last)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		allUsers = append(allUsers, usr)
-// 	}
-
-// 	fmt.Println(allUsers)
-// 	json.NewEncoder(w).Encode(allUsers)
-
-// 	fmt.Println("Success!")
-
-// }
-
-func getPeople(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != "GET" {
-		http.Error(w, "Must be a 'GET' request", 400)
-		return
-	}
-
-	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+func postPerson(w http.ResponseWriter, r *http.Request, last, first string) {
 
 	db, err := makeDBconnection()
 	if err != nil {
@@ -216,37 +139,168 @@ func getPeople(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT last, first from people order by last, first")
+	query := "insert into people (first, last) values ($1, $2)"
+	_, err = db.Exec(query, first, last)
 	if err != nil {
 		http.Error(w, "Something went wrong", 500)
 		return
 	}
 
-	allUsers := make([]userLastFirst, 0)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(fmt.Sprintf("Successfully inserted %s %s", first, last))
+
+}
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "GET" {
+		http.Error(w, "Must be a 'GET' request", 400)
+		return
+	}
+
+	db, err := makeDBconnection()
+	if err != nil {
+		http.Error(w, "Something went wrong", 500)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT last || ', ' || first as name from people order by last, first")
+	if err != nil {
+		http.Error(w, "Something went wrong", 500)
+		return
+	}
+
+	allNames := make([]string, 0)
 
 	for rows.Next() {
-		var usr userLastFirst
-		err = rows.Scan(&usr.Last, &usr.First)
+		var name string
+		err = rows.Scan(&name)
 		if err != nil {
 			http.Error(w, "Something went wrong", 500)
 			return
 		}
-		allUsers = append(allUsers, usr)
+		allNames = append(allNames, name)
 	}
 
-	fmt.Println(allUsers)
-	json.NewEncoder(w).Encode(allUsers)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(allNames)
 
 	fmt.Println("Success!")
 }
 
 type userDB struct {
 	ID    string `json:"id,omitempty"`
-	First string `json:"first"`
 	Last  string `json:"last"`
+	First string `json:"first"`
 }
 
 type userLastFirst struct {
 	Last  string `json:"last"`
 	First string `json:"first"`
 }
+
+// func queryPersonEndPoint(w http.ResponseWriter, r *http.Request) {
+
+// 	switch r.Method {
+// 	case "GET":
+// 		getPersonQueryString(w, r)
+// 	case "POST":
+// 		postPersonQueryString(w, r)
+// 	default:
+// 		http.Error(w, "Must be a 'GET' or 'POST' request", 400)
+// 		return
+// 	}
+
+// }
+
+// func postPersonQueryString(w http.ResponseWriter, r *http.Request) {
+
+// 	db, err := makeDBconnection()
+// 	if err != nil {
+// 		http.Error(w, "Something went wrong", 500)
+// 		return
+// 	}
+// 	defer db.Close()
+
+// 	params := r.URL.Query()
+
+// 	last, lastok := params["last"]
+// 	first, firstok := params["first"]
+
+// 	if !lastok || !firstok {
+// 		http.Error(w, "Please include 'first' and 'last' as a query parameter", 400)
+// 		return
+// 	}
+
+// 	query := "insert into people (first, last) values ($1, $2)"
+// 	_, err = db.Exec(query, first[0], last[0])
+// 	if err != nil {
+// 		http.Error(w, "Something went wrong", 500)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(fmt.Sprintf("Successfully inserted %s %s", first[0], last[0]))
+
+// }
+
+// func getPersonQueryString(w http.ResponseWriter, r *http.Request) {
+
+// 	db, err := makeDBconnection()
+// 	if err != nil {
+// 		http.Error(w, "Something went wrong", 500)
+// 		return
+// 	}
+// 	defer db.Close()
+
+// 	params := r.URL.Query()
+
+// 	last, lastok := params["last"]
+// 	first, firstok := params["first"]
+
+// 	if !lastok && !firstok {
+// 		http.Error(w, "Please include 'first' and/or 'last' as a query parameter", 400)
+// 		return
+// 	}
+
+// 	query := ""
+
+// 	if lastok && firstok {
+// 		query = fmt.Sprintf("SELECT id, first, last from people where first = '%s' and last = '%s'", first[0], last[0])
+// 	} else if lastok {
+// 		query = fmt.Sprintf("SELECT id, first, last from people where last = '%s'", last[0])
+// 	} else {
+// 		query = fmt.Sprintf("SELECT id, first, last from people where first = '%s'", first[0])
+// 	}
+
+// 	rows, err := db.Query(query)
+// 	if err != nil {
+// 		fmt.Println(err)
+
+// 		http.Error(w, "Something went wrong", 500)
+// 		return
+// 	}
+
+// 	allUsers := make([]userDB, 0)
+
+// 	for rows.Next() {
+// 		var usr userDB
+// 		err = rows.Scan(&usr.ID, &usr.Last, &usr.First)
+// 		if err != nil {
+// 			fmt.Println(err)
+
+// 			http.Error(w, "Something went wrong", 500)
+// 			return
+// 		}
+// 		allUsers = append(allUsers, usr)
+// 	}
+
+// 	if len(allUsers) == 0 {
+// 		http.Error(w, "No data found", 404)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(allUsers)
+// }
